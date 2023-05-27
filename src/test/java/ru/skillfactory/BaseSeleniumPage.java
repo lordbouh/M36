5a292fb3b668e158cc4104f33d05d54620d4987c
lordbouh <lordbouh@yahoo.com>
2023-05-27 19:42:22 +0400

    20 Tests on Java.

--- src/test/java/ru/skillfactory/BaseSeleniumPage.java
+++ src/test/java/ru/skillfactory/BaseSeleniumPage.java
@@ -21,7 +21,44 @@ public class BaseSeleniumPage {
     private static final String SUBSCRIBE_NEWS_IT_BUTTON = "//*[@id=\"form596444548\"]/div[2]/div[4]/button";
     private static final String COMPANY_LEARNING = "//*[@id=\"sbs-562561178-1678713806333\"]/div/a";
     private static final String CHECK_COMPANY_LEARNING = "//*[@id=\"rec425993788\"]/div/div/div[3]/h1/span";
+    private static final String PROGRAMMING_MAIN_PAGE = "//*[@id=\"rec562561178\"]/div/div/div[25]/a";
+    private static final String PROGRAMMING_JAVA = "//*[@id=\"rec569086576\"]/div/div/div[5]/a";
+    private static final String JAVA_COURSES = "//*[@id=\"rec520650424\"]/div/div/div/h1";
+    private static final String DATA_SCIENCE_MAIN_PAGE = "//*[@id=\"rec562561178\"]/div/div/div[32]/a";
+    private static final String ARTIFICIAL_INTELLIGENCE = "//*[@id=\"rec569092786\"]/div/div/div[7]/a";
+    private static final String ARTIFICIAL_INTELLIGENCE_TEXT = "//*[@id=\"rec569089586\"]/div/div/div[3]/h1";
+    private static final String DATA_ANALYST_CHECK = "//*[@id=\"rec453742605\"]/div/div/div[4]/div";
+    private static final String HIGN_EDUCATION_MAIN_PAGE = "//*[@id=\"rec562561178\"]/div/div/div[53]/a";
+    private static final String HIGH_EDUCATION_CHECK = "//*[@id=\"rec407825981\"]/div/div/div[3]/h1";
 
+    private static final String ALL_COURSES_MAIN_PAGE = "//*[@id=\"rec562561178\"]/div/div/div[20]/a";
+    private static final String BACKEND_DEVELOPER = "//*[@id=\"rec567539569\"]/div/div/div[18]/a";
+    private static final String BACKEND_DEVELOPER_CHECK = "//*[@id=\"rec565298354\"]/div/div/div[3]/h1";
+    private static final String WEB_DEVELOPING = "//*[@id=\"rec567539569\"]/div/div/div[16]/a";
+    private static final String WEB_DEVELOPING_CHECK = "//*[@id=\"rec569093451\"]/div/div/div[3]/h1";
+    private static final String APPLICATION_DEVELOPING = "//*[@id=\"rec567539569\"]/div/div/div[15]/a";
+    private static final String APPICATION_DEVELOPING_CHECK = "//*[@id=\"rec565292716\"]/div/div/div[3]/h1";
+    private static final String GAMES_DEVELOPING = "//*[@id=\"rec567539569\"]/div/div/div[14]/a";
+    private static final String GAMES_DEVELOPING_CHECK = "//*[@id=\"rec520594043\"]/div/div/div/h1";
+    private static final String SECURITY_COURSES = "//*[@id=\"rec567539569\"]/div/div/div[8]/a";
+    private static final String SECURITY_COURSES_CHECK = "//*[@id=\"rec565290318\"]/div/div/div[3]/h1";
+    private static final String IT_WITHOUT_CODE = "//*[@id=\"rec567539569\"]/div/div/div[23]/a";
+    private static final String IT_WITHOUT_CODE_CHECK = "//*[@id=\"rec555662258\"]/div/div/div/h1";
+    private static final String MARKETING_COURSES = "//*[@id=\"rec567539569\"]/div/div/div[9]/a";
+    private static final String MARKETING_COURSES_CHECK = "//*[@id=\"rec520590635\"]/div/div/div/h1";
+    private static final String MANAGEMENT_COURSES = "//*[@id=\"rec567539569\"]/div/div/div[11]/a";
+    private static final String MANAGEMENT_COURSES_CHECK = "//*[@id=\"rec520598643\"]/div/div/div/h1";
+    private static final String SUBSCRIBE_NEWS_IT_ERROR_MESSAGE = "//*[@id=\"tilda-popup-for-error\"]/div[1]/p";
+    private static final String PYTHON_COURSES = "//*[@id=\"rec569086576\"]/div/div/div[18]/a";
+    private static final String PYTHON_DEVELOPER = "//*[@id=\"rec569090284\"]/div/div/div[3]/a";
+    private static final String SIGN_UP_FOR_A_COURSE_BUTTON = "//*[@id=\"rec521548955\"]/div/div/div[5]/a";
+    private static final String SIGN_UP_FOR_A_COURSE_NAME_FIELD = "//*[@id=\"form521548955\"]/div[2]/div[1]/div/input";
+    private static final String SIGN_UP_FOR_A_COURSE_EMAIL_FIELD = "//*[@id=\"form521548955\"]/div[2]/div[2]/div/input";
+    private static final String SIGN_UP_FOR_A_COURSE_PHONE_FIELD = "//*[@id=\"form521548955\"]/div[2]/div[3]/div/div[1]/input[2]";
+    private static final String SIGN_UP_FOR_A_COURSE_CONFIRM_BUTTON = "//*[@id=\"form521548955\"]/div[2]/div[7]/button";
+    private static final String CONTACTS_MAIN_PAGE = "//*[@id=\"sbs-562561178-1678713806326\"]/div/a";
+    private static final String INFO_EMAIL_CONTACTS = "//*[@id=\"rec472344041\"]/div/div/div[6]/div/a";
+    private static final String EMAIL_BOSS_SF = "//*[@id=\"rec472344041\"]/div/div/div[8]/div/a";
     public void subscription_field() {
         driver.findElement(By.xpath(SUBSCRIPTION_NAME_FIELD)).sendKeys("Abra Kadabra");
         driver.findElement(By.xpath(SUBSCRIPTION_EMAIL)).sendKeys("abra@kadabra.au");
@@ -67,6 +104,148 @@ public class BaseSeleniumPage {
         return
      driver.findElement(By.xpath(CHECK_COMPANY_LEARNING)).getText();
     }
+    public void programming_main_page(){
+        driver.findElement(By.xpath(PROGRAMMING_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(PROGRAMMING_JAVA)).click();
+    }
+    public String java_courses(){
+        return
+                driver.findElement(By.xpath(JAVA_COURSES)).getText();
+    }
+    public void data_science_ai(){
+        driver.findElement(By.xpath(DATA_SCIENCE_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(ARTIFICIAL_INTELLIGENCE)).click();
+    }
+    public String artificial_intelligence_check(){
+        return
+        driver.findElement(By.xpath(ARTIFICIAL_INTELLIGENCE_TEXT)).getText();
+    }
+    public String data_analyst_check(){
+        return
+        driver.findElement(By.xpath(DATA_ANALYST_CHECK)).getText();
+    }
+    public void high_education(){
+        driver.findElement(By.xpath(HIGN_EDUCATION_MAIN_PAGE)).click();
+    }
+    public String high_education_check(){
+        return
+                driver.findElement(By.xpath(HIGH_EDUCATION_CHECK)).getText();
+    }
+    public void backend_developer(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(BACKEND_DEVELOPER)).click();
+    }
+    public String backend_developer_check(){
+        return
+                driver.findElement(By.xpath(BACKEND_DEVELOPER_CHECK)).getText();
+    }
+
+    public void web_developing(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(WEB_DEVELOPING)).click();
+    }
+    public String web_developing_check(){
+        return
+                driver.findElement(By.xpath(WEB_DEVELOPING_CHECK)).getText();
+    }
+
+    public void application_developing(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(APPLICATION_DEVELOPING)).click();
+    }
+
+    public String application_developing_check(){
+        return
+                driver.findElement(By.xpath(APPICATION_DEVELOPING_CHECK)).getText();
+    }
+    public void games_developing(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(GAMES_DEVELOPING)).click();
+
+    }
+    public String games_developing_check(){
+        return
+                driver.findElement(By.xpath(GAMES_DEVELOPING_CHECK)).getText();
+    }
+    public void security_courses(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(SECURITY_COURSES)).click();
+    }
+    public String security_courses_check(){
+        return
+                driver.findElement(By.xpath(SECURITY_COURSES_CHECK)).getText();
+    }
+    public void it_without_code_courses(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(IT_WITHOUT_CODE)).click();
+    }
+    public String it_without_code_courses_check(){
+        return
+                driver.findElement(By.xpath(IT_WITHOUT_CODE_CHECK)).getText();
+    }
+
+    public void marketing_courses(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(MARKETING_COURSES)).click();
+    }
+    public String marketing_courses_check(){
+        return
+                driver.findElement(By.xpath(MARKETING_COURSES_CHECK)).getText();
+    }
+    public void management_courses(){
+        driver.findElement(By.xpath(ALL_COURSES_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(MANAGEMENT_COURSES)).click();
+    }
+    public String management_courses_check(){
+        return
+                driver.findElement(By.xpath(MANAGEMENT_COURSES_CHECK)).getText();
+    }
+
+    public void subscribe_news_it_incorrect_email(){
+        driver.findElement(By.xpath(SUBSCRIBE_NEWS_IT)).sendKeys("fjff.fjf@fkfk");
+        driver.findElement(By.xpath(SUBSCRIBE_NEWS_IT_BUTTON)).click();
+    }
+    public String subscribe_it_news_incorrect_email_check(){
+        return
+                driver.findElement(By.xpath(SUBSCRIBE_NEWS_IT_ERROR_MESSAGE)).getText();
+    }
+
+    public void try_courses_incorrect_email(){
+        driver.findElement(By.xpath(PROGRAMMING_MAIN_PAGE)).click();
+        driver.findElement(By.xpath(PYTHON_COURSES)).click();
+    }
+    public void choose_python_developer(){
+        driver.findElement(By.xpath(PYTHON_DEVELOPER)).click();
+    }
+    public void sign_up_for_a_course_button(){
+        driver.findElement(By.xpath(SIGN_UP_FOR_A_COURSE_BUTTON)).click();
+    }
+    public void sign_up_for_a_course_name(){
+        driver.findElement(By.xpath(SIGN_UP_FOR_A_COURSE_NAME_FIELD)).sendKeys("Abra Kadabra");
+    }
+
+    public void sign_up_for_a_course_email(){
+        driver.findElement(By.xpath(SIGN_UP_FOR_A_COURSE_EMAIL_FIELD)).sendKeys("fsdsd@ssdsds");
+    }
+
+    public void sign_for_a_course_phone(){
+        driver.findElement(By.xpath(SIGN_UP_FOR_A_COURSE_PHONE_FIELD)).sendKeys("9332344343");
+    }
+    public void sign_for_a_course_confirm_button(){
+        driver.findElement(By.xpath(SIGN_UP_FOR_A_COURSE_CONFIRM_BUTTON)).click();
+    }
+    public void email_info(){
+        driver.findElement(By.xpath(CONTACTS_MAIN_PAGE)).click();
+    }
+    public Object email_info_check(){
+        return
+        driver.findElement(By.xpath(INFO_EMAIL_CONTACTS)).getTagName();
+        }
+        public Object email_boss(){
+        return
+        driver.findElement(By.xpath(EMAIL_BOSS_SF)).getTagName();
+        }
+
 }
 
 
--- src/test/java/ru/skillfactory/SiteTests.java
+++ src/test/java/ru/skillfactory/SiteTests.java
@@ -2,10 +2,7 @@ package ru.skillfactory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -52,12 +49,12 @@ private BaseSeleniumPage baseSeleniumPage;
         return whNow.iterator().next();
     }
     @Test
-        public void Test1_clickName() {
+        public void test1_clickName() {
       baseSeleniumPage.subscription_field();
         Assert.assertEquals("Please fill out all required fields",baseSeleniumPage.subscription_error_message());
     }
     @Test
-    public void Test2_sign_up_for_a_course() {
+    public void test2_sign_up_for_a_course() {
 
         baseSeleniumPage.Course_Testing();
         {
@@ -87,14 +84,143 @@ private BaseSeleniumPage baseSeleniumPage;
     }
 
     @Test
-    public void subscribe_it_news(){
+    public void test3_subscribe_it_news(){
         baseSeleniumPage.subscribe_news_it();
             }
 
     @Test
-    public void courses_for_business(){
+    public void test4_courses_for_business(){
         baseSeleniumPage.company_learning();
         Assert.assertEquals("Развивайте бизнес", baseSeleniumPage.check_company_learning());
     }
+    @Test
+    public void test5_courses_java(){
+        baseSeleniumPage.programming_main_page();
+        Assert.assertEquals("Курсы Java",baseSeleniumPage.java_courses());
+    }
+    @Test
+    public void test6_artificial_intelligence_course(){
+        baseSeleniumPage.data_science_ai();
+        Assert.assertEquals("Искусственный интеллект: курсы и профессии",baseSeleniumPage.artificial_intelligence_check());
+    }
+    @Test
+    public void test7_data_analyst(){
+        {
+            WebElement element = driver.findElement(By.cssSelector(".tn-elem__5625611781679404895615 > .tn-atom"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        driver.findElement(By.cssSelector(".tn-elem__5625611781679404895615 > .tn-atom")).click();
+        {
+            WebElement element = driver.findElement(By.cssSelector(".tn-elem__5690922001679953431614 > .tn-atom"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        vars.put("window_handles", driver.getWindowHandles());
+        driver.findElement(By.cssSelector(".tn-elem__5690922001679953431614 > .tn-atom")).click();
+        vars.put("win9546", waitForWindow(2000));
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        driver.switchTo().window(vars.get("win9546").toString());
+        Assert.assertEquals("Аналитик данных",baseSeleniumPage.data_analyst_check());
+    }
+    @Test
+    public void test8_high_education(){
+        baseSeleniumPage.high_education();
+        Assert.assertEquals("высшее образование в лучших университетах страны — онлайн", baseSeleniumPage.high_education_check());
+    }
 
+    @Test
+    public void test9_backend_developer(){
+        baseSeleniumPage.backend_developer();
+        Assert.assertEquals("Курсы по Backend-разработке", baseSeleniumPage.backend_developer_check());
+    }
+
+    @Test
+    public void test10_web_developing(){
+        baseSeleniumPage.web_developing();
+        Assert.assertEquals("Курсы по веб-разработке", baseSeleniumPage.web_developing_check());
+    }
+    @Test
+    public void test11_application_developing(){
+        baseSeleniumPage.application_developing();
+        Assert.assertEquals("Курсы по разработке мобильных приложений",baseSeleniumPage.application_developing_check());
+    }
+
+    @Test
+    public void test12_games_developing(){
+        baseSeleniumPage.games_developing();
+        Assert.assertEquals("Курсы по разработке игр",baseSeleniumPage.games_developing_check());
+    }
+
+    @Test
+    public void test13_security_courses(){
+        baseSeleniumPage.security_courses();
+        Assert.assertEquals("Курсы по информационной безопасности: обучение кибербезопасности", baseSeleniumPage.security_courses_check());
+    }
+
+    @Test
+    public void test14_it_without_code_courses(){
+        baseSeleniumPage.it_without_code_courses();
+        Assert.assertEquals("IT без кода", baseSeleniumPage.it_without_code_courses_check());
+    }
+
+    @Test
+    public void test15_marketing_courses(){
+        baseSeleniumPage.marketing_courses();
+        Assert.assertEquals("Курсы маркетинга", baseSeleniumPage.marketing_courses_check());
+    }
+
+    @Test
+    public void test16_management_courses(){
+        baseSeleniumPage.management_courses();
+        Assert.assertEquals("Курсы по менеджменту и управлению",baseSeleniumPage.management_courses_check());
+    }
+
+    @Test
+    public void test17_subscribe_news_it_incorrect_email(){
+        baseSeleniumPage.subscribe_news_it_incorrect_email();
+        Assert.assertEquals("Please enter a valid email address", baseSeleniumPage.subscribe_it_news_incorrect_email_check());
+    }
+
+    @Test
+    public void test18_python_course_incorrect_email() {
+        baseSeleniumPage.try_courses_incorrect_email();
+        vars.put("window_handles", driver.getWindowHandles());
+        baseSeleniumPage.choose_python_developer();
+        vars.put("win3009", waitForWindow(2000));
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        driver.switchTo().window(vars.get("win3009").toString());
+        baseSeleniumPage.sign_up_for_a_course_button();
+        baseSeleniumPage.sign_up_for_a_course_name();
+        baseSeleniumPage.sign_up_for_a_course_email();
+        baseSeleniumPage.sign_for_a_course_phone();
+        baseSeleniumPage.sign_for_a_course_confirm_button();
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+    }
+
+    @Test
+    public void test19_email_to_bosses(){
+        baseSeleniumPage.email_info();
+        Assert.assertEquals("a",baseSeleniumPage.email_info_check());
+    }
+
+    @Test
+    public void test20_email_boss_SF(){
+        baseSeleniumPage.email_info();
+        Assert.assertEquals("a",baseSeleniumPage.email_boss());
+    }
 }
+
+
